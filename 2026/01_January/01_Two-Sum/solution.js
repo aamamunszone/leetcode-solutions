a0fff71b3ad54631f114b3fa5f002828b320cf99
Abdullah Al Mamun <aamamunszone@gmail.com>
2026-01-01 20:36:01 +0600

    Day 01: Solved Two Sum (Easy) - Array, Hash Table

---
+++ 2026/01_January/01_Two-Sum/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return [];
+};
+
+// Test Cases
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
---
+++ 2026/01_January/01_Two-Sum/solution.md
@@ -0,0 +1,13 @@
+# 1. Tow-Sum
+
+## 📊 Complexity Analysis
+
+- **Time Complexity:** O(n) - Shudhu ekbar loop
+- **Space Complexity:** O(n) - Map aaa n ta element store
+
+---
+
+## 🎓 Key Learnings
+
+- [learn from this problem]
+- [which concept clear]
